d15f2e226aaafd48ab93423fbcba31ce5482e9c5
Quackster <quacksterr.15@gmail.com>
2023-01-12 20:30:44 +1000

    remove web app

--- AvataraWebApp/compile.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-dotnet publish --self-contained -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish --self-contained -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
